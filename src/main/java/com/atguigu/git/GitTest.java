5ac6e8c07d5983ac890fcad156abf51d73c16c2e
xbq <xbq@qq.com>
2022-08-25 15:38:11 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * Author:qq
+ * Date:2022/8/25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
